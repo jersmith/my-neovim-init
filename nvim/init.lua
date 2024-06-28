4f67ace351273603935e440375e606bc2fed3e9f
Jeremy <brisk.idea1238@fastmail.com>
2024-06-27 18:34:27 -0700

    fix(picker): work around for multi-select
    Adds a function for getting the multiple selections from the Telescope
    picker and invokes on <CR>. Works like fzf now.
    
    Fix from here: https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-1679797700

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,6 +12,9 @@ require('lualine').setup {
   }
 }
 
+-- color scheme
+vim.cmd[[colorscheme tokyonight]]
+
 -- setup telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -19,6 +22,29 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- color scheme
-vim.cmd[[colorscheme tokyonight]]
+-- function to allow multi-selection in the telescope picker
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
 
+-- the keybinding for telescope selection in the picker
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<CR>'] = select_one_or_multi,
+      }
+    }
+  }
+}
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -13,3 +13,7 @@ opt.list = true
 -- disable netrw 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- we don't use these either, tree sitter tries to load by default
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
